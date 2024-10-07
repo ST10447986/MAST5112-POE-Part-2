988af889cc3fa38a1893a2fa9c99eca5a46faa46
Hredae Moodley <st10447986@vcconnect.edu.za>
2024-10-07 19:55:18 +0200

    Navigation V2

---
+++ christv1/screens/index.tsx
@@ -0,0 +1,9 @@
+import { HomeScreen as Home } from "./homeScreen";
+import { EnterScreen as MenuEnter } from "./enterScreen";
+import { MenuView } from "./viewScreen";
+
+export {
+    Home,
+    MenuEnter,
+    MenuView
+};
